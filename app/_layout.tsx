f3ea5116736b7f744f1b8c23d63ad7ce8a6b2e82
david37432 <davidjimenez3743@gmail.com>
2025-02-26 16:39:06 -0500

    Ya todo funcionando

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack
-    initialRouteName="welcome"
-    >
+  return   <Stack
+    screenOptions={{headerShown:false}}>
       <Stack.Screen name="index" options={{ title: "Home" }} />
       <Stack.Screen name="splashscreen" options={{ title: "Splash", headerShown: false }} />
-      <Stack.Screen name="welcome" options={{ title: "Welcome" }} />
     </Stack>
-  );
+  
 }
